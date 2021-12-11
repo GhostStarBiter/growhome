fd67c00e76b8a9b99615ed62a2d5ae2716234de1
Dm.Zelenets <dm.zelenets@gmail.com>
2021-12-11 18:53:18 +0200

    Work on ESP connectivity State Machine

--- src/modules/esp/esp.c
+++ src/modules/esp/esp.c
@@ -1,43 +1,64 @@
 #include "stm32f10x.h"
 #include "task.h"
 
-#include "configuration/peripherals_config.h"
-
 #include "stm32f10x_usart.h"
 
 #include "gpio/mcu_gpio.h"
 #include "uart/mcu_uart.h"
 #include "utils/utils.h"
+#include "system_user_interface/sui.h"
 
 #include "string.h"
 #include "esp.h"
 
-#define ESP_SEND_BUF_SIZE                   512
+#define ESP_SEND_BUF_SIZE                   2048
 #define ESP_MAX_COMMAND_LEN                 (ESP_SEND_BUF_SIZE - AT_COMMAND_START_INDEX - sizeof(AT_REQEST_ENDING))
 
-#define ESP_RECV_BUF_SIZE                   512
+#define ESP_RECV_BUF_SIZE                   2048
 
-#define ESP_RESPONSE_TIMEOUT                500   // [ms]
+#define ESP_RECV_TIMEOUT_MAX                1000  // [ms]
 
 #define ESP_COMPARE_RESPONSE_BUF_SIZE       64    // [bytes]
 
 #define AT_COMMAND_START_INDEX              2     // position in buffer "AT+[command]"
 
+#define ESP_RECV_BUFFER_NOT_FULL            (esp_chip.recv_size < ESP_RECV_BUF_SIZE)
 
-#define ESP_RECV_BUFFER_EMPTY               (!esp.recv_size)
-#define ESP_RECV_BUFFER_NOT_FULL            (esp.recv_size < ESP_RECV_BUF_SIZE)
+typedef enum {
+  ESP_NOSTATE,
+  ESP_HW_RESET,
+  ESP_AT_RESET,
+  ESP_READY,
+  ESP_PREP_CONNECT,
+  ESP_CONNECTED,
+  ESP_PROCESS_RESPONSE,
+  ESP_INCOMING_DATA,
+  ESP_TUNELLING,
+  ESP_ERROR
+} esp_state_t;
+
+typedef enum {
+  NOT_RECV,
+  BY_IDLE,
+  BY_TIMEOUT,
+  INCOMING_DATA
+} esp_recv_end_t;
 
 typedef struct{
-    FunctionalState           chip_status;
+    USART_TypeDef*            interface;
+    FunctionalState           status;
     esp_state_t               state;
-    bool                      data_received;
+    bool                      recv_finished;
     uint8_t                   send_buf[ESP_RECV_BUF_SIZE];
     uint16_t                  send_size;
     uint8_t                   recv_buf[ESP_SEND_BUF_SIZE];
     uint16_t                  recv_size;
+    esp_recv_end_t            recv_type;
     void                      (*process_incoming_data)(uint8_t*);
 } esp_struct_t;
 
+
+static void esp_send_at_command(const char* const command);
 
 //--------------------------------------------------------------------------------------------------
 /// @brief Put received over UART data to receive buffer
@@ -53,30 +74,39 @@ static void esp_set_state(esp_state_t e_new_sw_status);
 //--------------------------------------------------------------------------------------------------
 /// @brief Wait ESP_RESPONSE_TIMEOUT ms or less if data receive finished
 //--------------------------------------------------------------------------------------------------
-static result_t esp_timeout_wait_receive_finished(void);
+static result_t esp_timeout_wait_receive_finished(esp_recv_end_t recv_finished, uint16_t timeout);
 
 //--------------------------------------------------------------------------------------------------
 /// @brief
 //--------------------------------------------------------------------------------------------------
-result_t esp_search_response(char* search, char* buffer);
-
-
-
-//  *****************
-esp_struct_t esp;
+result_t esp_search_in_buf(char* search, char* buffer);
 
 
 //--------------------------------------------------------------------------------------------------
-void esp_init(void (*callback_process_incoming_data)(uint8_t*))
+/// @brief
+//--------------------------------------------------------------------------------------------------
+static result_t esp_initiate_network_access(char*, char*);
+
+
+//  *****************
+esp_struct_t esp_chip;
+
+
+//--------------------------------------------------------------------------------------------------
+void esp_init(USART_TypeDef* esp_uart_interface, void (*callback_process_incoming_data)(uint8_t*))
 {
-  esp.chip_status = DISABLE;
-  esp.state = ESP_RESET;
-  esp.process_incoming_data = callback_process_incoming_data;
+  // check of NULL pointers
+  while(!esp_uart_interface);
+  while(!callback_process_incoming_data);
 
-  mcu_uart_set_dma_buffer_address((uint32_t) &esp.send_buf);
+  esp_chip.interface = esp_uart_interface;
+  esp_chip.process_incoming_data = callback_process_incoming_data;
 
-  memset(esp.send_buf, 0, ESP_SEND_BUF_SIZE);
+  esp_chip.status = DISABLE;
+  esp_chip.state = ESP_HW_RESET;
+  esp_chip.recv_type = BY_TIMEOUT;
 
+  memset(esp_chip.send_buf, 0, ESP_SEND_BUF_SIZE);
 
   esp_update();
 }
@@ -90,7 +120,7 @@ static void esp_reset_and_enable_chip(void)
 
   mcu_gpio_set_esp_enable_status(ENABLE);
   vTaskDelay(50);
-  esp.chip_status = ENABLE;
+  esp_chip.status = ENABLE;
 
   mcu_gpio_set_esp_reset_status(DISABLE);
 
@@ -98,29 +128,37 @@ static void esp_reset_and_enable_chip(void)
   vTaskDelay(1000);
 }
 
+
 //--------------------------------------------------------------------------------------------------
 void esp_update(void)
 {
   result_t operation_result;
 
-  switch(esp.state)
+  switch(esp_chip.state)
   {
-    case ESP_RESET:
+    case ESP_HW_RESET:
       esp_reset_and_enable_chip();
+      esp_set_state(ESP_AT_RESET);
+      break;
 
-      memcpy(esp.send_buf, AT_COMMAND_PREFIX, sizeof(AT_COMMAND_PREFIX));
-      strcat((char*) esp.send_buf, AT_RESET);
-      strcat((char*) esp.send_buf, AT_REQEST_ENDING);
+    case ESP_AT_RESET:
+      esp_send_at_command(AT_RESET);
 
-      esp.send_size = strlen((char*) esp.send_buf);
-
-      mcu_uart_dma_start_transmit(esp.send_size);
-
-      operation_result = esp_timeout_wait_receive_finished();
+      operation_result = esp_timeout_wait_receive_finished(BY_TIMEOUT, 500);
 
       if(operation_result)
       {
-        operation_result = esp_search_response("ready", (char*) esp.recv_buf);
+        operation_result = esp_search_in_buf("ready", (char*) esp_chip.recv_buf);
+
+        operation_result == result_success ? esp_set_state(ESP_READY) : esp_set_state(ESP_ERROR);
+      }
+
+      if(operation_result)
+      {
+        esp_send_at_command(AT_ECHO_OFF);
+        operation_result = esp_timeout_wait_receive_finished(BY_TIMEOUT, 10);
+
+        operation_result = esp_search_in_buf("OK", (char*) esp_chip.recv_buf);
 
         operation_result == result_success ? esp_set_state(ESP_READY) : esp_set_state(ESP_ERROR);
       }
@@ -128,20 +166,42 @@ void esp_update(void)
       break;
 
     case ESP_READY:
+      esp_send_at_command(ESP_SET_WIFI_MODE_SOFT_AP);
+
+      operation_result = esp_search_in_buf("OK", (char*) esp_chip.recv_buf);
+      operation_result == result_success ? esp_set_state(ESP_PREP_CONNECT) : esp_set_state(ESP_ERROR);
 
       break;
 
-    case ESP_INCOMING_DATA:
-
-      if(esp.data_received)
+    case ESP_PREP_CONNECT:
+      // wait for activation of connection procedure
+      if(sui_user_enter_credentials())
       {
-        esp.process_incoming_data(esp.recv_buf);
-        esp_set_state(ESP_READY);
+        char* user_ssid = sui_get_user_ssid();
+        char* user_pswd = sui_get_user_pswd();
+        operation_result = esp_initiate_network_access(user_ssid, user_pswd);
+        if(operation_result == result_success)
+          {
+            esp_set_state(ESP_CONNECTED);
+          }
+        else
+        {
+          esp_set_state(ESP_ERROR);
+        }
       }
       else
       {
         vTaskDelay(1);
       }
+      break;
+
+    case ESP_CONNECTED:
+      break;
+
+    case ESP_INCOMING_DATA:
+      esp_timeout_wait_receive_finished(BY_IDLE, 0);
+      esp_chip.process_incoming_data(esp_chip.recv_buf);
+      esp_set_state(ESP_CONNECTED);
       break;
 
     case ESP_ERROR:
@@ -156,23 +216,59 @@ void esp_update(void)
 
 
 //--------------------------------------------------------------------------------------------------
-static result_t esp_timeout_wait_receive_finished(void)
+static void esp_send_at_command(const char* const command)
 {
-  static uint32_t counter = 0;
+  esp_chip.recv_finished = false;
+
+  memcpy(esp_chip.send_buf, AT_COM, strlen(AT_COM));
+  strcat((char*) esp_chip.send_buf, command);
+  strcat((char*) esp_chip.send_buf, AT_REQEST_ENDING);
+
+  esp_chip.send_size = strlen((char*) esp_chip.send_buf);
+
+  mcu_uart_dma_start_transmit(esp_chip.interface, (uint32_t) &esp_chip.send_buf[0], esp_chip.send_size);
+}
+
+
+//--------------------------------------------------------------------------------------------------
+static result_t esp_timeout_wait_receive_finished(esp_recv_end_t recv_finished_type, uint16_t timeout_ms)
+{
+  uint16_t counter = 0;
   result_t  result = result_success;
 
-  while(!esp.data_received)
+  switch(recv_finished_type)
   {
-    vTaskDelay(1);
-    counter++;
-
-    if(counter > ESP_RESPONSE_TIMEOUT)
+  case BY_IDLE:
+    while(!esp_chip.recv_finished)
     {
-      counter = 0;
-      esp.state = ESP_ERROR;
-      result = result_fail;
-      break;
+      vTaskDelay(1);
+      counter++;
+
+      if(counter > ESP_RECV_TIMEOUT_MAX)
+      {
+        esp_chip.state = ESP_ERROR;
+        result = result_fail;
+        break;
+      }
     }
+    break;
+
+  case BY_TIMEOUT:
+    while(counter < timeout_ms)
+    {
+      vTaskDelay(1);
+      counter++;
+    };
+
+    if(!esp_chip.recv_finished)
+    {
+      esp_chip.state = ESP_ERROR;
+      result = result_fail;
+    }
+    break;
+
+  default:
+    break;
   }
 
   return result;
@@ -180,7 +276,32 @@ static result_t esp_timeout_wait_receive_finished(void)
 
 
 //--------------------------------------------------------------------------------------------------
-result_t esp_search_response(char* search, char* buffer)
+static result_t esp_initiate_network_access(char* ssid, char* pswd)
+{
+  result_t result = result_success;
+
+  char tmp_buf[50];
+  memset((void*) tmp_buf, 0, 50);
+
+  strcat(tmp_buf, ESP_JOIN_AP);
+  strcat(tmp_buf, "\"");    // add first ["]
+  strcat(tmp_buf, ssid);
+  strcat(tmp_buf, "\",\""); // add second ["] and [,"]
+  strcat(tmp_buf, pswd);
+  strcat(tmp_buf, "\"");    // add closing ["]
+
+  esp_send_at_command(tmp_buf);
+
+  result = esp_timeout_wait_receive_finished(BY_TIMEOUT, 100);
+
+  result = esp_search_in_buf("OK", (char*) esp_chip.recv_buf);
+
+  return result;
+}
+
+
+//--------------------------------------------------------------------------------------------------
+result_t esp_search_in_buf(char* search, char* buffer)
 {
   result_t result = result_fail;
 
@@ -193,26 +314,26 @@ result_t esp_search_response(char* search, char* buffer)
 //--------------------------------------------------------------------------------------------------
 static void esp_set_state(esp_state_t new_state)
 {
-  esp.state = new_state;
+  esp_chip.state = new_state;
 }
 
 
 //--------------------------------------------------------------------------------------------------
-static void esp_receive_data(uint8_t byte)
+static void esp_receive_data(uint8_t data)
 {
   uint16_t index;
 
-  if(ESP_RECV_BUFFER_NOT_FULL && !ESP_RECV_BUFFER_EMPTY)
+  if(ESP_RECV_BUFFER_NOT_FULL)
   {
-    index = esp.recv_size;
+    index = esp_chip.recv_size;
   }
   else
   {
-    index = esp.recv_size = 0;
+    index = esp_chip.recv_size = 0;
   }
 
-  esp.recv_size++;
-  esp.recv_buf[index] = byte;
+  esp_chip.recv_size++;
+  esp_chip.recv_buf[index] = data;
 }
 
 
@@ -220,25 +341,50 @@ static void esp_receive_data(uint8_t byte)
 void ESP_INTERFACE_IRQ_HANDLER(void)
 {
   uint8_t data;
-  static bool ssid_name_started = false;
 
   //  RECEIVE DATA
   if(USART_GetITStatus(ESP_UART_INTERFACE, USART_IT_RXNE) == SET)
   {
     USART_ClearFlag(ESP_UART_INTERFACE, USART_IT_RXNE);
-    USART_ITConfig(ESP_UART_INTERFACE, USART_IT_IDLE, ENABLE);
 
-    data = (uint8_t) USART_ReceiveData(ESP_UART_INTERFACE);
+    switch(esp_chip.recv_type)
+    {
+    case BY_IDLE:
+      USART_ITConfig(ESP_UART_INTERFACE, USART_IT_IDLE, ENABLE);
+      data = (uint8_t) USART_ReceiveData(ESP_UART_INTERFACE);
+      esp_receive_data(data);
+      break;
 
-    esp_receive_data(data);
+    case BY_TIMEOUT:
+      data = (uint8_t) USART_ReceiveData(ESP_UART_INTERFACE);
+      esp_receive_data(data);
+      break;
+
+    case NOT_RECV:
+      (void) USART_ReceiveData(ESP_UART_INTERFACE);
+      break;
+
+    case INCOMING_DATA:
+      esp_set_state(ESP_INCOMING_DATA);
+      data = (uint8_t) USART_ReceiveData(ESP_UART_INTERFACE);
+      esp_receive_data(data);
+      break;
+
+    default:
+      break;
+    }
   }
+
 
   //  DATA RECEIVING FINISHED
   if(USART_GetITStatus(ESP_UART_INTERFACE, USART_IT_IDLE) == SET)
   {
     USART_ClearFlag(ESP_UART_INTERFACE, USART_IT_IDLE);
 
-    esp.data_received = true;
+    if(esp_chip.recv_type == BY_IDLE)
+    {
+      esp_chip.recv_finished = true;
+    }
 
     USART_ITConfig(ESP_UART_INTERFACE, USART_IT_IDLE, DISABLE);
 
--- src/modules/esp/esp.h
+++ src/modules/esp/esp.h
@@ -4,27 +4,15 @@
 #include "main/types.h"
 #include "stm32f10x.h"
 
+#include "configuration/peripherals_config.h"
+
 #include "at_commands.h"
-
-
-typedef enum {
-  ESP_NOSTATE,
-  ESP_RESET,
-  ESP_WAIT_READY,
-  ESP_READY,
-  ESP_LIST_AP,
-  ESP_CONNECT,
-  ESP_PROCESS_RESPONSE,
-  ESP_INCOMING_DATA,
-  ESP_TUNELLING,
-  ESP_ERROR
-} esp_state_t;
 
 
 //------------------------------------------------------------------------------
 /// @brief Function to init ESP8266 module
 //------------------------------------------------------------------------------
-void esp_init(void (*callback_process_incoming_data)(uint8_t*));
+void esp_init(USART_TypeDef* esp_uart_interface, void (*callback_process_incoming_data)(uint8_t*));
 
 
 //------------------------------------------------------------------------------
