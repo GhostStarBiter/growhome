9879d87990c7e11d9c780edd859f953446fd8dda
Dm.Zelenets <dm.zelenets@gmail.com>
2022-03-03 17:19:31 +0200

    WATER config: changed pump default power.

--- src/configuration/water_pump_config.h
+++ src/configuration/water_pump_config.h
@@ -2,7 +2,7 @@
 #define WATER_PUMP_CONFIG_H
 
 
-#define WATER_PUMP_DEFAULT_POWER            40  // [%]
+#define WATER_PUMP_DEFAULT_POWER            50  // [%]
 #define WATER_TANK_MINIMAL_LEVEL            10  // [%]
 
 #endif // WATER_PUMP_CONFIG_H
