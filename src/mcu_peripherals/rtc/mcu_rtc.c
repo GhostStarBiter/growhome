539210dccd1ed7f3e2d2cc5df5378bd9d2c9d53f
Dmytro Zelenets <dm.zelenets@gmail.com>
2022-09-21 00:51:56 +0300

    Change mcu_rtc module: delete private header; add day member; add day getter function.

--- src/mcu_peripherals/rtc/mcu_rtc.c
+++ src/mcu_peripherals/rtc/mcu_rtc.c
@@ -1,5 +1,16 @@
-#include "mcu_rtc_priv.h"
+#include "stm32f10x.h"
+#include "system_config/stm32f10x_conf.h"
+#include "mcu_rtc.h"
 
+#define HOURS_PER_DAY                     24
+
+#define SECONDS_PER_HOUR                  3600
+
+typedef struct {
+  uint32_t    counter;
+  mcu_time_t  current;
+  int         day;
+} mcu_rtc_t;
 
 volatile mcu_rtc_t mcu_rtc;
 
@@ -18,6 +29,7 @@ void mcu_rtc_update(void)
 
   if(mcu_rtc.counter >= (HOURS_PER_DAY*SECONDS_PER_HOUR))
   {
+    mcu_rtc.day++;
     mcu_rtc_reset_counter();
   }
 
@@ -44,6 +56,13 @@ void mcu_rtc_set_time(mcu_time_t set_time)
   }
 
   mcu_rtc.counter = set_time.hour*3600 + set_time.min*60 + set_time.sec;
+}
+
+
+//--------------------------------------------------------------------------------------------------
+uint16_t mcu_rtc_get_day(void)
+{
+  return mcu_rtc.day;
 }
 
 
